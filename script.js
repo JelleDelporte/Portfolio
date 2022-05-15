a9e541c12ec468a519efb225750af898050d813e
Jelle Delporte <jelle.delporte@student.hogent.be>
2022-05-15 13:39:36 +0200

    .

---
+++ script.js
@@ -0,0 +1,52 @@
+gsap.from(".a", {
+  opacity: 0,
+  x: -300,
+  ease: "power2.out",
+  duration: 1.3,
+  scrollTrigger: {
+    trigger: ".a",
+    toggleActions: "restart complete restart complete",
+  }
+})
+
+gsap.from(".b", {
+  opacity: 0,
+  x: 300,
+  ease: "power2.out",
+  duration: 1.3,
+  scrollTrigger: {
+    trigger: ".b",
+    toggleActions: "restart complete restart restart",
+  }
+})
+
+gsap.from(".c", {
+  opacity: 0,
+  x: -300,
+  ease: "power2.out",
+  duration: 1.3,
+  scrollTrigger: {
+    trigger: ".c",
+    toggleActions: "restart complete restart restart",
+  }
+})
+
+gsap.from(".d", {
+  opacity: 0,
+  x: 300,
+  ease: "power2.out",
+  duration: 1.3,
+  scrollTrigger: {
+    trigger: ".d",
+    toggleActions: "restart complete restart restart",
+  }
+})
+
+
+
+
+// https://www.youtube.com/watch?v=X7IBa7vZjmo
+
+// https://codepen.io/GreenSock/pen/bGexQpq
+
+// https://greensock.com/docs/v3/GSAP/Timeline
